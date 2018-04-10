8f5fdd71e0ace8d3194b52ccf4716786324a7209
mucheng <454941218@qq.com>
2018-04-10 23:08:34 +0800

    sddd

---
+++ Desktop/code/IDEA/mybatistest/src/main/java/com/oracle/mybatistest/MybatistestApplication.java
@@ -0,0 +1,12 @@
+package com.oracle.mybatistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatistestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatistestApplication.class, args);
+	}
+}
